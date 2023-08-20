5f12f87456ac566b606384e0fbadcafaff1b554a
tingprado <ethel.prado1998@gmail.com>
2023-08-20 14:02:21 +0800

    Removed space

--- index.js
+++ index.js
@@ -26,4 +26,4 @@ const server = http.createServer((req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => console.log("Server running at port ", PORT));
+server.listen(PORT, () => console.log("Server running at port", PORT));
